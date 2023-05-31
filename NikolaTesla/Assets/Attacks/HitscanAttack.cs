8553fc2132628ec7be9b4ee811b1f1ea1d8e0df3
rosaParvum <rosaparvum@gmail.com>
2023-05-31 21:27:41 +0100

    so much level implementation

--- NikolaTesla/Assets/Attacks/HitscanAttack.cs
+++ NikolaTesla/Assets/Attacks/HitscanAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HitscanAttack : Attack {
     public GameObject decal;
     public float range;
+    public float kineticEnergy;
 
     public void fired() {
         Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width/2,Screen.height/2));
@@ -19,9 +20,12 @@ public class HitscanAttack : Attack {
 
         GameObject objectHit = hit.transform.gameObject;
     
-        if (objectHit.tag != "Enemy") {
-            Instantiate(decal,hit.point,Quaternion.FromToRotation(Vector3.forward,hit.normal));
-        } else if (objectHit.tag == "Enemy") {
+        if (!objectHit.CompareTag("Enemy")) {
+            Instantiate(decal,hit.point,Quaternion.FromToRotation(Vector3.forward,hit.normal)).transform.SetParent(hit.transform);
+            if (objectHit.CompareTag("prop")) {
+                objectHit.GetComponent<Rigidbody>().AddExplosionForce(100f*kineticEnergy,hit.point,5f);
+            }
+        } else if (objectHit.CompareTag("Enemy")) {
             objectHit.SendMessage("hit",this.damage);
         }
     }
